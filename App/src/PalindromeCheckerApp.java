516f612c4567528a11910c10b77afd7595b82515
Shi-vangi00 <sp5317@srmist.edu.in>
2026-02-25 11:16:33 +0530

    Shivangi added UseCase7

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,29 +1,29 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
 public class PalindromeCheckerApp {
     public static void main(String[] args){
-        String input = "madam";
+        String input = "deified";
 
-        // Convert string to character array
-        char[] characters = input.toCharArray();
-
-        // Two-pointer approach
-        int start = 0;
-        int end = characters.length - 1;
+        Deque<Character> deque = new ArrayDeque<>();
         boolean isPalindrome = true;
 
-        while (start < end) {
-            if (characters[start] != characters[end]) {
+        for (char c : input.toCharArray()) {
+            deque.addLast(c);
+        }
+
+        while (deque.size() > 1) {
+            char first = deque.removeFirst();
+            char last = deque.removeLast();
+
+            if (first != last) {
                 isPalindrome = false;
                 break;
             }
-            start++;
-            end--;
         }
 
-        // Display result
         System.out.println("Input String : " + input);
-
         if (isPalindrome) {
-            System.out.println("Result : It is a Palindrome.");
+            System.out.println("Result : It is a Deque-validated Palindrome.");
         } else {
             System.out.println("Result : It is NOT a Palindrome.");
         }
